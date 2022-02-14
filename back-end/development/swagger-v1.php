3fb5d072a80d6b5b50156cafdbba4af4f9924dab
hamongkhang <khang.ha22@student.passerellesnumeriques.org>
2022-02-14 22:27:42 +0700

    set up swagger

---
+++ back-end/development/swagger-v1.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @SWG\Swagger(
+ *     schemes={"http"},
+ *     host=API_HOST,
+ *     basePath="/",
+ *     @SWG\Info(
+ *         version="1.0.0",
+ *         title="Laravel and Swagger",
+ *         description="Getting started with Laravel and Swagger",
+ *         termsOfService="",
+ *         @SWG\Contact(
+ *             email="name@example.com"
+ *         ),
+ *     ),
+ * )
+ */
