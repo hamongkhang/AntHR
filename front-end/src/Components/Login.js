5b90e50bbc074be5f86cf5c5561686d77c3f593c
Dang Thi Thu Sang <sang22@pnvd142.pn.com.vn>
2022-02-19 07:32:50 -0800

    add Loginforms

---
+++ front-end/src/Components/Login.js
@@ -0,0 +1,37 @@
+import React, { Component} from "react";
+
+import { Form, Button } from "react-bootstrap";
+export default class Login extends Component {
+  render() {
+    return (
+      <div className="container mt-5">
+        <div className="mt-5 " id="loginState">
+          <Form className="formLogin">
+            <h3>Login to your account</h3>
+            <Form.Group className="mb-3 mt-3" controlId="formBasicEmail">
+              <Form.Label>Email Adress*</Form.Label>
+              <Form.Control type="email" placeholder="Email Address" />
+            </Form.Group>
+            <Button
+              type="submit"
+              className={"btnLogin"}
+              style={{
+                backgroundColor: "#2ad25f",
+                border: "1px solid #2ad25f",
+              }}
+            >
+              Continue
+            </Button>
+          </Form>
+          <center className="mt-3">
+            Don't have an account?
+            <a href="" style={{ color: "#2ad25f", textDecoration: "none" }}>
+              {" "}
+              Sign Up
+            </a>
+          </center>
+        </div>
+      </div>
+    );
+  }
+}
