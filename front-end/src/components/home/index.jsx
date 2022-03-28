888e2411bfc69394a187c96209556dae45ca62e1
hamongkhang <khang.ha22@student.passerellesnumeriques.org>
2022-03-28 18:05:45 +0700

    fix: account components

--- front-end/src/components/home/index.jsx
+++ front-end/src/components/home/index.jsx
@@ -4,7 +4,7 @@ import Home from '../home/home';
 import Login from '../login';
 import ForgotPassword from "../forgot_password";
 import Register from "../register";
-import ActiveAccount from "../Account";
+import ActiveAccount from "../account";
 
 const HomePages = (props) => {
     const { changeRender } = props;
